7606fd31147746bb9bf37e5fe8deee8611335c0b
alexander garza <garza.alexdev@gmail.com>
2026-07-01 00:24:01 -0400

    fix(api) singularActivty does not exist

--- apps/api/src/endpoints/activity/index.ts
+++ apps/api/src/endpoints/activity/index.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import { pluralActivityRouter } from "./plural";
-import { singularActivityRouter } from "./singular";
 
 export const activityRouter = Router();
 
 activityRouter.use(pluralActivityRouter);
-activityRouter.use(singularActivityRouter);
